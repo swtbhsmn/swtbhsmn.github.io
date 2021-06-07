1e0b6f466ec52f15d8762a4d3dc4d4cbe9f6ae5e
swtbhsmn <swtbhsmn@gmail.com>
2021-06-07 20:49:32 +0530

    route connected

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const imageHolder = document.querySelector('.backdrop');
 window.addEventListener('scroll', () => {
 
 
-    if(window.scrollY>=100){
+    if(window.scrollY>=50){
+      
         if(imageHolder){
             imageHolder.classList.add('show-backdrop')
             imageHolder.classList.remove('hide-backdrop')
         }
     }else{
+       
         if(imageHolder){
             imageHolder.classList.remove('show-backdrop')
             imageHolder.classList.add('hide-backdrop')
