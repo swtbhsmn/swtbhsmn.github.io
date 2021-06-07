20f98f4ba9717b50e4a546ba0d1e4282a3715025
swtbhsmn <swtbhsmn@gmail.com>
2021-06-07 12:55:27 +0530

    layout ready

---
+++ index.js
@@ -0,0 +1,24 @@
+const imageHolder = document.querySelector('.backdrop');
+
+
+window.addEventListener('scroll', () => {
+
+
+    if(window.scrollY>=100){
+        if(imageHolder){
+            imageHolder.classList.add('show-backdrop')
+            imageHolder.classList.remove('hide-backdrop')
+        }
+    }else{
+        if(imageHolder){
+            imageHolder.classList.remove('show-backdrop')
+            imageHolder.classList.add('hide-backdrop')
+        }
+    }
+  
+
+
+})
+
+
+
